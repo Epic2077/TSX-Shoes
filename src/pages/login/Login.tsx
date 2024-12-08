aafce9de05dea15c0d23a4a5b854d62f89a14227
Ashkan <the.epic.2077@gmail.com>
2024-12-09 00:30:52 +0330

    Feat(Login): Updating Login Page
    Updating Login Page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Back from "../../layout/header.tsx/back";
 
 const LoginPage: React.FC = () => {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const emailInputRef = useRef<HTMLInputElement>(null);
   const passwordInputRef = useRef<HTMLInputElement>(null);
+
+  const handleFocus = (inputRef: React.RefObject<HTMLInputElement>) => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+      inputRef.current.classList.add("border-2", "border-black");
+    }
+  };
+
+  const handleBlur = (inputRef: React.RefObject<HTMLInputElement>) => {
+    if (inputRef.current) {
+      inputRef.current.classList.remove("border-2", "border-black");
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+
   return (
     <div className="px-6 py-[12px]">
       <Back address="/" />
@@ -14,7 +33,13 @@ const LoginPage: React.FC = () => {
         Login to Your Account
       </h1>
       <form action="" className="mt-11">
-        <div className="w-full h-[37px] bg-[#FAFAFA] flex p-[13px] items-center">
+        {/* Email Input */}
+        <div
+          className="w-full h-[37px] bg-[#FAFAFA] flex p-[13px] items-center rounded mb-[21px]"
+          onFocus={() => handleFocus(emailInputRef)}
+          onBlur={() => handleBlur(emailInputRef)}
+          ref={emailInputRef}
+        >
           <img src="../../../src/assets/icons/email.svg" alt="email" />
           <input
             type="email"
@@ -23,23 +48,31 @@ const LoginPage: React.FC = () => {
             placeholder="Email"
           />
         </div>
-        <div className="w-full h-[37px] bg-[#FAFAFA] flex p-[13px] items-center">
+
+        {/* Password Input */}
+        <div
+          className="w-full h-[37px] bg-[#FAFAFA] flex p-[13px] items-center rounded"
+          onFocus={() => handleFocus(passwordInputRef)}
+          onBlur={() => handleBlur(passwordInputRef)}
+          ref={passwordInputRef}
+        >
           <img src="../../../src/assets/icons/lock.svg" alt="pass" />
           <input
-            type="password"
+            type={isPasswordVisible ? "text" : "password"}
             name="password"
             className="p-[4px] bg-transparent h-[24px] w-full outline-none"
             placeholder="Password"
-            id="pass"
           />
           <img
             src="../../../src/assets/icons/eye.svg"
-            alt="hide"
+            alt="toggle visibility"
             className="cursor-pointer"
+            onClick={togglePasswordVisibility}
           />
         </div>
       </form>
     </div>
   );
 };
+
 export default LoginPage;
