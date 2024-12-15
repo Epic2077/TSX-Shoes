ede95c1276459606b03c46d9f8b1a24b727d3180
Ashkan <the.epic.2077@gmail.com>
2024-12-15 16:41:03 +0330

    Feat(Home): Added Brand Logos

---
+++ src/components/home-components/brandPage/brandArray.tsx
@@ -0,0 +1,42 @@
+export const BrandArray = [
+  {
+    name: "Nike",
+    images: "../../../../src/assets/brands/nike.png",
+    fullName: "Nike",
+  },
+  {
+    name: "Adidas",
+    images: "../../../../src/assets/brands/Adidas.png",
+    fullName: "Adidas",
+  },
+  {
+    name: "Puma",
+    images: "../../../../src/assets/brands/Puma.png",
+    fullName: "Puma",
+  },
+  {
+    name: "Asics",
+    images: "../../../../src/assets/brands/Asics.png",
+    fullName: "Asics",
+  },
+  {
+    name: "Reebok",
+    images: "../../../../src/assets/brands/Reebok.png",
+    fullName: "Reebok",
+  },
+  {
+    name: "New Ba..",
+    images: "../../../../src/assets/brands/New-Balance.png",
+    fullName: "New-Balance",
+  },
+  {
+    name: "Converse",
+    images: "../../../../src/assets/brands/Converse.png",
+    fullName: "Converse",
+  },
+  {
+    name: "More ..",
+    images: "../../../../src/assets/brands/More.png",
+    fullName: "More",
+  },
+];
--- src/pages/home/Index.tsx
+++ src/pages/home/Index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeHeader from "../../components/home-components/header/header";
 import HomeSearch from "../../components/home-components/search/Search";
+import HomeBrand from "../../components/home-components/brandPage/Brands";
 
 const Home: React.FC = () => {
   return (
     <div className="px-6 py-4">
       <HomeHeader />
       <HomeSearch />
+      <HomeBrand />
     </div>
   );
 };
