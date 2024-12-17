8646f22e5d7ec53b22376f4be099586fd21c1438
arashmogh <arash.ms1994@gmail.com>
2024-12-17 23:28:06 +0330

    Feat: Build Products/arash

---
+++ src/api/Api.ts
@@ -0,0 +1,15 @@
+import { Product } from "../types/type";
+import Api from "./Base";
+
+export async function getProducts(params?: { id: number }) {
+  return (
+    await Api.get<Product[]>("/products", {
+      params,
+    })
+  ).data;
+}
+
+export async function getProductById(id: number) {
+  const res = await Api.get<Product>("/products/" + id);
+  return res.data;
+}
---
+++ src/components/home-components/products/product-card/ProductCard.components.tsx
@@ -0,0 +1,29 @@
+import { Product } from "../../../../types/Product.type";
+import { Link } from "react-router-dom";
+
+interface Props {
+  product: Product;
+}
+
+const ProductCard = ({ product }: Props) => {
+  return (
+    <Link
+      to={`/products/${product.id}`}
+      className="flex flex-col items-start justify-start pb-1"
+    >
+      <div className="rounded-3xl w-[182px] h-[182px]">
+        <img
+          src={product.images}
+          alt={product.title}
+          className="w-full h-full rounded-3xl"
+        />
+      </div>
+      <h1 className="font-bold text-xl text-[#152536] mt-2">{product.title}</h1>
+      <p className="text-base font-semibold text-[#152536] mb-2">
+        $ {product.price}
+      </p>
+    </Link>
+  );
+};
+
+export default ProductCard;
