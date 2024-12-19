932d67b5a1540549dc1a174cf204301b82878239
Ashkan <the.epic.2077@gmail.com>
2024-12-19 22:30:34 +0330

    Feat(Filteration): Added Array Functionality to Filter Buttons

--- src/components/home-components/products/Product.tsx
+++ src/components/home-components/products/Product.tsx
@@ -3,25 +3,32 @@ import { BrandArray } from "../brandPage/brandArray";
 import FilterBtn from "../filter-button/btn";
 
 const HomeProducts: React.FC = () => {
-  const [activeFilter, setActiveFilter] = useState<string>("All");
+  const [activeFilter, setActiveFilter] = useState<string[]>([]);
 
   const handleFilterClick = (filter: string) => {
-    //Array
-    setActiveFilter(filter);
+    if (filter === "All") {
+      setActiveFilter([]);
+    } else {
+      setActiveFilter((prevFilters) =>
+        prevFilters.includes(filter)
+          ? prevFilters.filter((f) => f !== filter)
+          : [...prevFilters, filter]
+      );
+    }
   };
   return (
     <>
       <div className="flex gap-[12px] overflow-x-auto max-w-full mt-3 overflow-y-hidden py-2">
         <FilterBtn
           text="All"
-          isActive={activeFilter === "All"}
+          isActive={activeFilter.length === 0}
           onClick={() => handleFilterClick("All")}
         />
         {BrandArray.map((brand) => (
           <FilterBtn
             key={brand.name}
             text={brand.fullName}
-            isActive={activeFilter === brand.name}
+            isActive={activeFilter.includes(brand.name)}
             onClick={() => handleFilterClick(brand.name)}
           />
         ))}
