2d8191113976799c858d7e6a1b7c496bc6126a48
alinamdari79 <90065028+alinamdari79@users.noreply.github.com>
2024-12-20 17:45:13 +0330

    create search box and header

--- src/layout/search-box/searchBox.tsx
+++ src/layout/search-box/searchBox.tsx
@@ -3,8 +3,34 @@ import React from "react";
 const SearchBox: React.FC = () => {
   return (
     <>
-      <div>
-        <input type="text" name="" id="" placeholder="search"/>
+      <div className="flex justify-between mx-6 my-4">
+        <div>
+          <span className="font-bold text-lg">9:41</span>
+        </div>
+        <div className="flex gap-1">
+          <img src="/src/assets/icons/cellular-icon.svg" alt="cellular-icon" />
+          <img src="/src/assets/icons/wifi-icon.svg" alt="wifi-icon" />
+          <img src="/src/assets/icons/battery-icon.svg" alt="battery-icon" />
+        </div>
+      </div>
+      <div className="flex h-9 items-center bg-[#FAFAFA] py-8 m-6 rounded-3xl border-2 hover:border-zinc-800">
+        <img
+          src="/src/assets/icons/search.svg"
+          alt="search-icon"
+          className="pl-6 h-6"
+        />
+        <input
+          type="text"
+          name="search-box"
+          id="search-box"
+          placeholder="Search"
+          className="bg-transparent w-full h-9 outline-none pl-4 text-lg"
+        />
+        <img
+          src="/src/assets/icons/setting-icon.svg"
+          alt="setting-icon"
+          className="pr-4"
+        />
       </div>
     </>
   );
