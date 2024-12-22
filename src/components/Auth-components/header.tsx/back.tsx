094cb91be7e7b1956e7a2d1643677c2404d0ad34
Ashkan <the.epic.2077@gmail.com>
2024-12-22 17:45:47 +0330

    Feat(Checkout): Added CheckoutPage

--- src/components/Auth-components/header.tsx/back.tsx
+++ src/components/Auth-components/header.tsx/back.tsx
@@ -19,7 +19,7 @@ const Back: React.FC<backProps> = ({ address, name }) => {
 
   return (
     <div
-      className="w-8 h-8 flex justify-center items-center cursor-pointer"
+      className="w-8 h-8 flex justify-center items-center cursor-pointer w-full"
       onClick={handleNavigation}
     >
       <img src="../../../src/assets/icons/back.svg" alt="back" />
