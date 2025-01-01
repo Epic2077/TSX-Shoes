33435b56c7676e9f6fe9ee0a8bed566dac1508d1
mahsa sadr <mahsamisr@gmail.com>
2025-01-01 20:23:49 +0330

    first commit

--- src/layout/search-box/searchBox.tsx
+++ src/layout/search-box/searchBox.tsx
@@ -3,22 +3,20 @@ import ProductsContainer from "../../components/home-components/products/product
 import { useState, useEffect } from "react";
 
 const SearchBox: React.FC = () => {
-  const date = new Date();
-  const timeBar = date.getHours() + ":" + date.getMinutes();
+
 
   const [searchField, setSearchField] = useState("");
   const [products, setProducts] = useState([] as any[]);
   const [filteredProducts, setFilteredProducts] = useState(products);
 
   useEffect(() => {
-    fetch("http://localhost:3000/products")
+    fetch("http://localhost:8000/api/products")
       .then((Response) => Response.json())
-      .then((title) => setProducts(title));
-  }, []);
+      .then((name) => setProducts(name));  }, []);
 
   useEffect(() => {
     const newFilteredProducts = products.filter((product) => {
-      return product.title.toLocaleLowerCase().includes(searchField);
+      return product.name.toLocaleLowerCase().includes(searchField);
 
     });
     setFilteredProducts(newFilteredProducts);
@@ -31,17 +29,7 @@ const SearchBox: React.FC = () => {
 
   return (
     <>
-      <div className="flex justify-between mx-6 my-4">
-        <div>
-          {/* <span className="font-bold text-lg">9:41</span> */}
-          <span className="font-bold text-lg"> {timeBar}</span>
-        </div>
-        <div className="flex gap-1">
-          <img src="/src/assets/icons/cellular-icon.svg" alt="cellular-icon" />
-          <img src="/src/assets/icons/wifi-icon.svg" alt="wifi-icon" />
-          <img src="/src/assets/icons/battery-icon.svg" alt="battery-icon" />
-        </div>
-      </div>
+
       <div className="flex h-9 items-center bg-[#FAFAFA] py-8 m-6 rounded-3xl border-2 hover:border-zinc-800">
         <img
           src="/src/assets/icons/search.svg"
