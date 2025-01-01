f9e3083af036ecd0a649b0ef9dbbbc4ba76c651d
arashmogh <arash.ms1994@gmail.com>
2025-01-01 21:21:40 +0330

    Change CallBack with UseQuery

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { Provider } from "react-redux";
 import RouterPage from "./router/Router";
 import { persistor, store } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <RouterPage />
-        </PersistGate>
+        <QueryClientProvider client={queryClient}>
+          <PersistGate loading={null} persistor={persistor}>
+            <RouterPage />
+          </PersistGate>
+        </QueryClientProvider>
       </Provider>
     </>
   );
