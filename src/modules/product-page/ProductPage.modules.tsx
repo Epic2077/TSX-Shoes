2f4f70522ec2e3cb9b66444ae193bcf77556e855
arashmogh <arash.ms1994@gmail.com>
2025-01-03 16:33:15 +0330

    Fix issues

--- src/modules/product-page/ProductPage.modules.tsx
+++ src/modules/product-page/ProductPage.modules.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useProduct } from "../../api/Query";
-import Back from "../../components/Auth-components/header/back";
 import ProductDetails from "./ProductDetails.modules";
 import ProductQuantity from "./ProductQuantity.modules";
 import HomeFooter from "../../components/home-components/footer/Footer";
@@ -11,7 +10,7 @@ import { LoadingSpinner } from "../../components/loading-spinner/loading";
 import WishlistEvent from "../../components/wishlist/wishlist-event";
 import ProductImages from "./ProductImages.modules";
 import { useMutation } from "react-query";
-import { addToCart, useCartMutation } from "../../pages/cart/AddToCart";
+import { addToCart } from "../../pages/cart/AddToCart";
 import { CartItem } from "../../types/CartItem.type";
 
 // ======== Error Component ========
@@ -23,7 +22,7 @@ const ErrorComponent = ({ message }: { message: string }) => (
 
 const ProductPage = () => {
   const { id } = useParams<{ id: string }>();
-  const { product, isErrorProduct, isLoadingProduct } = useProduct(Number(id));
+  const { data: product, isError, isLoading } = useProduct(id);
   const [selectedSize, setSelectedSize] = useState<number | null>(null);
   const [selectedColor, setSelectedColor] = useState<string | null>(null);
 
