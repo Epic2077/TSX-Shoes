a916a8a583776ee7a9b85d13042fea5c4e88aaaf
alinamdari79 <90065028+alinamdari79@users.noreply.github.com>
2025-01-06 00:40:06 +0330

    aaa

---
+++ src/pages/cart/GetFromCart.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { BASE_URL } from "../../api/Base";
+import { CartItem } from "../../types/CartItem.type";
+import { useSelector } from "react-redux";
+interface RootState {
+  auth: {
+    token: string;
+  };
+}
+
+export async function getFromCart(accessToken: string): Promise<CartItem> {
+
+  try {
+    const response = await axios.get<CartItem>(`${BASE_URL}/api/cart`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching cart items", error);
+    throw error;
+  }
+}
